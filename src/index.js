1d68568e887545a309efb1755025fd8a0b88293e
yona3 <kou.y.0905@icloud.com>
2021-08-13 00:47:33 +0900

    add: checkbox

--- src/index.js
+++ src/index.js
@@ -3,20 +3,28 @@
 import $ from "jquery";
 import "./style.css";
 
+// global state
+const state = {
+  todos: [],
+};
+
 // initalize the app
 (async () => {
   // fetch the data
   const res = await fetch("http://localhost:3000/todos");
   const todos = await res.json();
+  state.todos = todos;
 
   const templateContent = $("#todo-template")[0].content;
   const fragment = document.createDocumentFragment();
 
   // add the todos to the DOM
-  $.each(todos, (_, todo) => {
+  $.each(state.todos, (_, todo) => {
     const clone = document.importNode(templateContent, true);
+    const $input = $("input", clone);
     const $li = $("li", clone);
     const $p = $("p", $li);
+    $input.attr("checked", todo.isDone);
     $li.attr("id", todo.id);
     $p.text(todo.title);
     fragment.appendChild(clone);
