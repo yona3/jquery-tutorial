bcef490f92d319c6ee4fdd630b448cc35ce79481
yona3 <kou.y.0905@icloud.com>
2021-08-13 02:03:15 +0900

    add: add todo

--- src/index.js
+++ src/index.js
@@ -8,6 +8,21 @@ const state = {
   todos: [],
 };
 
+const templateContent = $("#todo-template")[0].content;
+const $form = $("form");
+const $input = $("input", $form);
+
+const setTodoContent = (clone, data) => {
+  const $input = $("input", clone);
+  const $li = $("li", clone);
+  const $p = $("p", $li);
+
+  $input.attr("checked", data.isDone);
+  $li.attr("id", data.id);
+  $p.text(data.title);
+  return clone;
+};
+
 // initalize the app
 (async () => {
   // fetch the data
@@ -15,20 +30,55 @@ const state = {
   const todos = await res.json();
   state.todos = todos;
 
-  const templateContent = $("#todo-template")[0].content;
   const fragment = document.createDocumentFragment();
 
   // add the todos to the DOM
   $.each(state.todos, (_, todo) => {
     const clone = document.importNode(templateContent, true);
-    const $input = $("input", clone);
-    const $li = $("li", clone);
-    const $p = $("p", $li);
-    $input.attr("checked", todo.isDone);
-    $li.attr("id", todo.id);
-    $p.text(todo.title);
-    fragment.appendChild(clone);
+    const configuredClone = setTodoContent(clone, todo);
+    fragment.appendChild(configuredClone);
   });
 
   $("#todos").append(fragment);
 })();
+
+const addTodo = async (todo) => {
+  try {
+    // post the data
+    const res = await fetch("http://localhost:3000/todos", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(todo),
+    });
+    const data = await res.json();
+    if (!data) throw new Error("No data returned");
+
+    // add the todo to the DOM
+    const clone = document.importNode(templateContent, true);
+    const configuredClone = setTodoContent(clone, todo);
+    $("#todos").append(configuredClone);
+
+    state.todos.push(todo);
+    console.log(state.todos);
+
+    // clear the form
+    $input[0].value = "";
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+// add Todo
+$form.on("submit", async (e) => {
+  e.preventDefault();
+
+  const title = $input[0].value;
+  const todo = {
+    title,
+    isDone: false,
+  };
+
+  await addTodo(todo);
+});
