5a02171d2b549cf58d40ea62c1ec50ba87f83f53
yona3 <kou.y.0905@icloud.com>
2021-08-13 00:38:03 +0900

    add: initialize the app

--- src/index.js
+++ src/index.js
@@ -1,9 +1,26 @@
 "use strict";
 
+import $ from "jquery";
 import "./style.css";
 
+// initalize the app
 (async () => {
+  // fetch the data
   const res = await fetch("http://localhost:3000/todos");
   const todos = await res.json();
-  console.log("todos: ", todos);
+
+  const templateContent = $("#todo-template")[0].content;
+  const fragment = document.createDocumentFragment();
+
+  // add the todos to the DOM
+  $.each(todos, (_, todo) => {
+    const clone = document.importNode(templateContent, true);
+    const $li = $("li", clone);
+    const $p = $("p", $li);
+    $li.attr("id", todo.id);
+    $p.text(todo.title);
+    fragment.appendChild(clone);
+  });
+
+  $("#todos").append(fragment);
 })();
